5efcf4803bf822e63b004d5c487c68a166966c5e
JesseNiyo1 <46729915+JesseNiyo1@users.noreply.github.com>
2026-05-19 09:44:41 -0400

    Update main.py
    Fixed bug in is_ISS_nearby()

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def is_ISS_nearby():
     response.raise_for_status()
 
     data = response.json()
-    longitude = data["iss_position"]["longitude"]
-    latitude = data["iss_position"]["latitude"]
+    longitude = float(data["iss_position"]["longitude"])
+    latitude = float(data["iss_position"]["latitude"])
     return abs(LATITUDE - latitude) < 5 and abs(LONGITUDE - longitude) < 5
 
 
